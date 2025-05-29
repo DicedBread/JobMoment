5bf892e1f6717ea89a69f64a29c79b96aed807ae
DicedBread <benjijread@gmail.com>
2025-05-29 12:57:30 +1200

    maintains style

--- src/main/java/diced/bread/DocContainer.java
+++ src/main/java/diced/bread/DocContainer.java
@@ -2,15 +2,20 @@ package diced.bread;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.google.api.services.docs.v1.Docs;
+import com.google.api.services.docs.v1.model.BatchUpdateDocumentRequest;
+import com.google.api.services.docs.v1.model.BatchUpdateDocumentResponse;
 import com.google.api.services.docs.v1.model.Color;
+import com.google.api.services.docs.v1.model.DeleteContentRangeRequest;
 import com.google.api.services.docs.v1.model.Document;
 import com.google.api.services.docs.v1.model.InsertTextRequest;
 import com.google.api.services.docs.v1.model.Location;
 import com.google.api.services.docs.v1.model.Paragraph;
 import com.google.api.services.docs.v1.model.ParagraphElement;
+import com.google.api.services.docs.v1.model.Range;
 import com.google.api.services.docs.v1.model.Request;
 import com.google.api.services.docs.v1.model.StructuralElement;
 import com.google.api.services.docs.v1.model.TextStyle;
@@ -29,7 +34,7 @@ public class DocContainer {
 
     public void findThing(String docId) {
         try {
-            Document doc = docService.documents().get(docId).setIncludeTabsContent(true).execute();
+            Document doc = docService.documents().get(docId).execute();
             // DocumentTab tab = doc.getTabs().get(0).getDocumentTab();
 
             List<StructuralElement> bodyContent = doc.getBody().getContent();
@@ -47,6 +52,12 @@ public class DocContainer {
                 }
             });
 
+            ArrayList<Request> l = new ArrayList<>(test);
+            Collections.reverse(l);
+
+            BatchUpdateDocumentRequest req = new BatchUpdateDocumentRequest().setRequests(l);
+            BatchUpdateDocumentResponse res = docService.documents().batchUpdate(doc.getDocumentId(), req).execute();
+            
         } catch (IOException e) {
             System.out.println(e);
         }
@@ -57,6 +68,8 @@ public class DocContainer {
         int end = element.getEndIndex();
         String content = element.getTextRun().getContent();
         TextStyle style = element.getTextRun().getTextStyle();
+        // boolean isBold = style.getBold();
+
         if (style.getBackgroundColor() == null)
             return;
 
@@ -65,15 +78,34 @@ public class DocContainer {
         if (c.toString().equals(BLUE)) {
             // conditional
         } else {
+            String resString = "test";
+
+
+            boolean hasEndLine = content.charAt(content.length() - 1) == '\n';
+            int endIndex = (hasEndLine) ? end - 1 : end;
+            DeleteContentRangeRequest res = new DeleteContentRangeRequest()
+                    .setRange(
+                            new Range()
+                                    .setStartIndex(start)
+                                    .setEndIndex(endIndex));
+            test.add(new Request().setDeleteContentRange(res));
+
             test.add(new Request()
-                    .setInsertText(new InsertTextRequest().setText("test").setLocation(new Location().setIndex(end))));
+                    .setInsertText(
+                            new InsertTextRequest()
+                                    .setText(resString)
+                                    .setLocation(new Location().setIndex(endIndex))));
+
         }
 
-        System.out.println("start: " + start + " end: " + end + " length: " + content.length());
-        System.out.println(content.trim().strip());
-        System.out.println("color: " + c);
+        // System.out.println("start: " + start + " end: " + end + " length: " +
+        // content.length());
+        // System.out.println(content);
+        // System.out.println("color: " + c);
     }
 
+    private void replace(int startIndex, int endIndex, String newText, String currentContent) {
 
+    }
 
 }
